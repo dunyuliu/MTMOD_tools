03e57e0449b13c1898bca4a303b268843b61c0de
DUNYU LIU <dunyuliu@gmail.com>
2023-07-31 14:29:25 -0500

    Create MTMOD_env.2023additional.sh

---
+++ MTMOD_env.2023additional.sh
@@ -0,0 +1,5 @@
+
+conda install -c conda-forge -c anaconda -c defaults meshio=5.3.4 matplotlib=3.5.1 pandas=1.4.3
+conda install -c conda-forge -c anaconda -c defaults petsc=3.17 cmcrameri=1.4 
+conda install -c conda-forge -c anaconda -c defaults fenics=2019.1.0
+pip install gmshparser==0.2.0
