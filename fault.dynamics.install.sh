4f27b117d7659a2eb707191472d93bbeaff03870
DUNYU LIU <dunyuliu@gmail.com>
2023-08-04 10:07:31 -0500

    Update fault.dynamics.install.sh

--- fault.dynamics.install.sh
+++ fault.dynamics.install.sh
@@ -1,7 +1,8 @@
 rm -rf FaultDynamics.tar FaultDynamics
 mkdir FaultDynamics
 
-wget --no-check-certificate "https://utexas-my.sharepoint.com/:u:/g/personal/dliu_ig_utexas_edu/EYvuFuoVZm5HmgAqMPNVk5kBDOjqYDR1HjKivchlHFKMqA?e=Mcmfh0&download=1" -O FaultDynamics.tar
+wget --no-check-certificate "https://utexas-my.sharepoint.com/:u:/g/personal/dliu_ig_utexas_edu/ETNgb4u2othCqEzG9K8qsdcB7xMFbGRV5ByQofcrHXE9_A?e=v2Yu1c&download=1" -O FaultDynamics.tar
+#wget --no-check-certificate "https://utexas-my.sharepoint.com/:u:/g/personal/dliu_ig_utexas_edu/EYvuFuoVZm5HmgAqMPNVk5kBDOjqYDR1HjKivchlHFKMqA?e=Mcmfh0&download=1" -O FaultDynamics.tar
 # need to add $download=1 in the OneDrive link
 
 tar -xvf FaultDynamics.tar -C FaultDynamics
